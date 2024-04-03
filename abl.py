89b4a7abca3ed4987253ad30990d0d42e8f22b12
hmacipegit <hmacipe98@gmail.com>
2024-04-03 15:44:52 +0200

    Add option

--- abl.py
+++ abl.py
@@ -1,10 +1,10 @@
 from time import sleep
 from random import randint
 
-opt = ["Buena presentacion", "Buena presentación, jefe", "Mucho mejor que la del icm"]
+opt = ["Buena presentacion", "Buena presentación, jefe", "Mucho mejor que la del icm", "Mucho mejor que la del icm"]
 print("Buena presentación")
 sleep(3)
 while True:
-    affirmation = randint(0,2)
+    affirmation = randint(0,3)
     print(opt[affirmation])
     sleep(3)
