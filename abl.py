7a07d1f6bf51ac4d89e9fb9161bdb2df38b5f060
hmacipegit <hmacipe98@gmail.com>
2024-04-03 11:40:53 +0200

    Add first iteration

--- abl.py
+++ abl.py
@@ -2,6 +2,8 @@ from time import sleep
 from random import randint
 
 opt = ["Buena presentacion", "Buena presentación, jefe", "Mucho mejor que la del icm"]
+print("Buena presentación")
+sleep(3)
 while True:
     affirmation = randint(0,2)
     print(opt[affirmation])
